3f3d2f1d44a8d90b45ba4afac07311e017c47893
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-02-02 11:02:43 -0300

    component loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import  { ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens/Home';
 import theme from './src/theme';
+import { ActivityIndicator } from 'react-native';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      {
+        fontsLoaded ?  <Home /> : <Loading />
+      }
       <StatusBar style="auto" />
     </ThemeProvider>
   );
