7f8d764f53bf9b8e6804e2398fb8b4f99364ac73
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-02-02 11:06:36 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
 import theme from './src/theme';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {
         fontsLoaded ?  <Home /> : <Loading />
       }
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
