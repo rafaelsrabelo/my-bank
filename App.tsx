ada4a872baaaeaab2e452eeddc5d60cef9c8cb97
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-02-02 09:35:10 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#612f74',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
